b697666eb7c39b698cf3493961d1eb0422887c86
Bodhi <bodhidharma202@gmail.com>
2024-04-10 04:49:59 +0800

    Add project files.

---
+++ IOOP Assignment/Login.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace IOOP_Assignment
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtbxUsername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtbxUsername.Text;
+            string password = txbxPassword.Text;
+
+            string connectionString = Properties.Settings.Default.DRDatabaseConnectionString;
+
+            string query = "SELECT * FROM login_database WHERE username = @Username AND password = @Password";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Username", username);
+                        command.Parameters.AddWithValue("@Password", password);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                MessageBox.Show("Login succesful!");
+                            }
+                            else
+                            {
+                                MessageBox.Show("Invalid username or password");
+                            }
+                        }
+
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {ex.Message}");
+                }
+            }
+        }
+    }
+}
